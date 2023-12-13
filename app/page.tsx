48d2d1d54a8782ac81fd959b14c324b64a0f0413
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-12-13 05:16:40 -0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -53,21 +53,24 @@ export default function Home() {
           >
             Arcan
           </Link>
-          | <Link
+          | 
+          <Link
             target="_blank"
             href="https://github.com/broomva"
             className="underline duration-500 hover:text-zinc-300"
           >
             Github
           </Link>
-          | <Link
+          | 
+          <Link
             target="_blank"
             href="https://huggingface.co/Broomva"
             className="underline duration-500 hover:text-zinc-300"
           >
             HuggingFace
           </Link>
-          | <Link
+          | 
+          <Link
             target="_blank"
             href="https://www.linkedin.com/in/broomva/"
             className="underline duration-500 hover:text-zinc-300"
